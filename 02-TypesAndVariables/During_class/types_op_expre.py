667f239e779314ae18b08abc4d0e7e38e598df17
Maciej Jastrzębski <macio.evo@MacBook-Pro.local>
2022-10-10 11:37:25 +0200

    Please enter the commit message for your changes. Lines starting

---
+++ 02-TypesAndVariables/During_class/types_op_expre.py
@@ -0,0 +1,20 @@
+x = 3 - 2 + 1
+print(x)
+
+y = 5 + 10 * 5
+print(y)
+
+z = 4 + 4 / 2 ** 2
+print(z)
+
+d = 4 % 3 % 2 % 1
+print(d)
+
+e = 1 + 2 % 3 ** 4 * 5
+print(e)
+
+f = True != False
+print(f)
+
+
+
---
+++ 02-TypesAndVariables/During_class/variables.py
@@ -0,0 +1,16 @@
+n1 = 5
+n2 = 1 
+n3 = 8
+n4 = 6
+n5 = 3
+
+sum = n1 + n2 + n3 + n4 + n5
+squared_sum = n1 ** n1 + n2 ** n2 + n3 ** n3 + n4 ** n4 + n5 ** n5
+quotient = n3 / n5
+
+if n3 == n4:
+    print("True")
+else:
+    print("False")
+        
+
