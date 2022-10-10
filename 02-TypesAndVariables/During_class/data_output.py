d28b893cebee5b88e4fb80469b397ea3580a7c0e
Maciej Jastrzębski <macio.evo@MacBook-Pro.local>
2022-10-10 22:08:13 +0200

    a

---
+++ 02-TypesAndVariables/During_class/data_output.py
@@ -0,0 +1,13 @@
+# ex 11
+
+name = 'Maciek'
+height = 184
+age = 20
+
+print(f'My name is {name}, I\'m {age} years old, and my height is {height} cm')
+
+
+# ex 12
+
+x = 5
+print("The value is {}, and {} is it's second power".format(x, x*x))
---
+++ 02-TypesAndVariables/During_class/programs.py
@@ -0,0 +1,19 @@
+# determine radius and PI
+radius = 5
+PI = 3.14
+
+# calculate area 
+area = PI * radius ** 2
+print(area)
+
+# calculate circumference 
+circumference = 2 * PI * radius
+print(circumference)
+
+
+
+degrees_celsius = float(input("Enter degrees celsius: "))
+celsius_in_fahrenheit = degrees_celsius * 1.8 + 32
+celsius_in_kelvin = degrees_celsius + 273
+print("celsius_in_fahrenheit: ",celsius_in_fahrenheit)
+print("celsius_in_kelvin: ",celsius_in_kelvin)
