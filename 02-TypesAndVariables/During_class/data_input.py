81f88223bd961668e297e733e650e14fccb491c0
Maciej Jastrzębski <macio.evo@MacBook-Pro.local>
2022-10-10 12:15:20 +0200

    first exercise

---
+++ 02-TypesAndVariables/During_class/data_input.py
@@ -0,0 +1,6 @@
+Name = input("Enter your name: ")
+Surname = input("Enter your surname: ")
+
+
+print("Your name is:",Name,Surname)
+
--- 02-TypesAndVariables/During_class/variables.py
+++ 02-TypesAndVariables/During_class/variables.py
@@ -5,12 +5,30 @@ n4 = 6
 n5 = 3
 
 sum = n1 + n2 + n3 + n4 + n5
+print(sum)
+
 squared_sum = n1 ** n1 + n2 ** n2 + n3 ** n3 + n4 ** n4 + n5 ** n5
+print(squared_sum)
+
 quotient = n3 / n5
+print(quotient)
+
 
 if n3 == n4:
     print("True")
 else:
     print("False")
+
+
+x = 7
+y = 34
+
+z = 7
+x = 34
+y = 7
+
+print(x, y)
+
+
         
 
